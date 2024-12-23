24aabde13fe56832f3705920dcdf8c4624f983ad
yk-onwards <kos028548@gmail.com>
2024-12-23 11:09:49 +0900

    스마트공장사업관리시스템 로그 전송 추가

--- src/main/java/com/springboot/java_eco/controller/SignController.java
+++ src/main/java/com/springboot/java_eco/controller/SignController.java
@@ -2,6 +2,7 @@ package com.springboot.java_eco.controller;
 
 
 import ch.qos.logback.classic.Logger;
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.springboot.java_eco.data.dto.SignInResultDto;
 import com.springboot.java_eco.data.dto.SignUpResultDto;
 import com.springboot.java_eco.data.dto.common.CommonInfoSearchDto;
@@ -17,9 +18,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 
@@ -90,6 +99,7 @@ public class SignController {
 
         SignInResultDto signInResultDto = signService.signIn(user.getId(), user.getPassword(),request.getRemoteAddr());
         if(signInResultDto.getCode() == 0){
+            sendLogData("DO6001",user.getId());
             LOGGER.info("[signIn] 정상적으로 로그인되었습니다.id: {}, token : {}",user.getId(), signInResultDto.getToken());
         }
         return signInResultDto;
@@ -136,6 +146,64 @@ public class SignController {
 
         return new ResponseEntity<>(map,responseHeaders,httpStatus);
 
+    }
+
+    public static void sendLogData(String useSe, String sysUser) {
+        try {
+            // URL 설정
+            String urlString = "https://log.smart-factory.kr/apisvc/sendLogData.json";
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // HTTP 메서드와 헤더 설정
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Cookie", "WAF=f3d78604bf02bdb611efeb5cb9e5a8b2; elevisor_for_j2ee_uid=5p7q478x1z5cp");
+            conn.setDoOutput(true); // OutputStream 사용을 허용
+
+            // 고정 값
+            String crtfcKey = "$5$API$ET4JqhTkYv.EycGF/nNmNksuRCU6R1bBnkT6gXADNj3";
+            String conectIp = "115.68.194.157";
+            String dataUsgqty = "0";
+
+            // 현재 시간 생성 (YYYY-MM-DD HH:MM:SS.SSS)
+            String logDt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+
+            // JSON 데이터 생성
+            String jsonInputString = String.format(
+                    "{\n" +
+                            "  \"crtfcKey\" : \"%s\",\n" +
+                            "  \"logDt\" : \"%s\",\n" +
+                            "  \"useSe\" : \"%s\",\n" +
+                            "  \"sysUser\" : \"%s\",\n" +
+                            "  \"conectIp\" : \"%s\",\n" +
+                            "  \"dataUsgqty\" : \"%s\"\n" +
+                            "}", crtfcKey, logDt, useSe, sysUser, conectIp, dataUsgqty);
+
+            // JSON 데이터를 요청 본문에 쓰기
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // 응답 코드 확인
+            int code = conn.getResponseCode();
+            System.out.println(jsonInputString);
+            System.out.println("Response Code: " + code);
+
+            // 응답 읽기
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+
+            System.out.println("@@ " + response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/main/java/com/springboot/java_eco/controller/StockInoutController.java
+++ src/main/java/com/springboot/java_eco/controller/StockInoutController.java
@@ -7,6 +7,7 @@ import com.springboot.java_eco.data.dto.common.CommonResultDto;
 import com.springboot.java_eco.data.dto.common.CommonSearchDto;
 import com.springboot.java_eco.data.dto.stockInout.StockInoutDto;
 import com.springboot.java_eco.data.entity.StockInout;
+import com.springboot.java_eco.data.entity.User;
 import com.springboot.java_eco.service.StockInoutService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,18 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 @RequestMapping("/stock_inout")
@@ -64,6 +77,10 @@ public class StockInoutController {
     public CommonResultDto createStockInout(@RequestBody StockInoutDto stockInoutDto) throws Exception{
         long currentTime = System.currentTimeMillis();
         LOGGER.info("[stockInoutDto]  : {}", stockInoutDto);
+
+        User user = new User();
+        sendLogData("DO6007",stockInoutDto.getUser_id());
+
         CommonResultDto stockInoutResultDto = stockInoutService.saveStockInout(stockInoutDto);
         LOGGER.info("[createStockInout] response Time : {}ms", System.currentTimeMillis() - currentTime);
         return  stockInoutResultDto;
@@ -83,5 +100,63 @@ public class StockInoutController {
         return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
     }
 
+
+    public static void sendLogData(String useSe, String sysUser) {
+        try {
+            // URL 설정
+            String urlString = "https://log.smart-factory.kr/apisvc/sendLogData.json";
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // HTTP 메서드와 헤더 설정
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Cookie", "WAF=f3d78604bf02bdb611efeb5cb9e5a8b2; elevisor_for_j2ee_uid=5p7q478x1z5cp");
+            conn.setDoOutput(true); // OutputStream 사용을 허용
+
+            // 고정 값
+            String crtfcKey = "$5$API$ET4JqhTkYv.EycGF/nNmNksuRCU6R1bBnkT6gXADNj3";
+            String conectIp = "115.68.194.157";
+            String dataUsgqty = "0";
+
+            // 현재 시간 생성 (YYYY-MM-DD HH:MM:SS.SSS)
+            String logDt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+
+            // JSON 데이터 생성
+            String jsonInputString = String.format(
+                    "{\n" +
+                            "  \"crtfcKey\" : \"%s\",\n" +
+                            "  \"logDt\" : \"%s\",\n" +
+                            "  \"useSe\" : \"%s\",\n" +
+                            "  \"sysUser\" : \"%s\",\n" +
+                            "  \"conectIp\" : \"%s\",\n" +
+                            "  \"dataUsgqty\" : \"%s\"\n" +
+                            "}", crtfcKey, logDt, useSe, sysUser, conectIp, dataUsgqty);
+
+            // JSON 데이터를 요청 본문에 쓰기
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // 응답 코드 확인
+            int code = conn.getResponseCode();
+            System.out.println(jsonInputString);
+            System.out.println("Response Code: " + code);
+
+            // 응답 읽기
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+
+            System.out.println("@@ " + response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
